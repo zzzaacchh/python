ca5fdac4b52d9b29aae3596c62f121ff882ee917
Zach <38481977+zzzaacchh@users.noreply.github.com>
2021-03-29 17:08:08 -0500

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -1,66 +1,58 @@
 import plotly.graph_objects as go
 import plotly.express as px
+from plotly.subplots import make_subplots
 import pandas as pd
 import yfinance as yf
 from datetime import datetime, timedelta
 
 def moneyprinter(stock):
-     
-     # get current day and change format     
-     current_date = datetime.today().strftime('%Y-%m-%d') 
-     past_year = datetime.today() - timedelta(days=365) 
-     past_week = datetime.today() - timedelta(days=7)
-     
-     # prints graphs for each stock ticker in the stonks array
+     current_date = datetime.today().strftime('%Y-%m-%d')
+     past_year = datetime.today() - timedelta(days=365)
+     # prints graphs for every stock ticker in stonks[]
      for i in stock:
           stick = yf.Ticker(i)
           print(i, "\n"*2, stick.major_holders, "\n"*2)
           holders(i)
           printgraph(i, past_year.strftime('%Y-%m-%d'), current_date)
-          printgraph(i, past_week.strftime('%Y-%m-%d'), current_date)
 
 def holders(stk): 
      stack = yf.Ticker(stk)
      largestshareholder = "Largest Shareholders for " + stk
      # pie chart of institutional holders
      fig = px.pie(stack.institutional_holders, values='Shares', names='Holder', hole=.3)
-     fig.update_layout(title_text=largestshareholder, annotations=[dict(text=stk, x=0.5, y=0.5, font_size=20, showarrow=False)])
+     fig.update_layout(title_text=largestshareholder, 
+                       annotations=[dict(text=stk, x=0.5, y=0.5, 
+                       font_size=20, showarrow=False)])
      fig.show()
 
 
 def printgraph(stock, startdate, enddate):
-     
-     # downloads historical data into dataframe - reset_index() needed to use date column for some reason
+     # reset_index() is needed to use date column
      hist_df = yf.download(stock, start = startdate, end = enddate).reset_index()
-     # converts date to usable format
+     # convert date to usable format
      hist_df['Date'] = pd.to_datetime(hist_df['Date'])
-     # sorts by date
      hist_df = hist_df.sort_values('Date', ascending=True)
-     plotly_data = []
-     layout = []
-     # creates candlestick chart
-     plotly_data.append(go.Candlestick(x=hist_df['Date'], 
+     fig = make_subplots(rows=2, cols=1, shared_xaxes=True, 
+               vertical_spacing=0.03, subplot_titles=(stock, 'Volume'), 
+               row_width=[0.2, 0.7])
+     fig.add_trace(go.Candlestick(x=hist_df['Date'], 
                                         open=hist_df['Open'],
                                         high=hist_df['High'], 
                                         low=hist_df['Low'], 
                                         close=hist_df['Close'], 
                                         increasing_line_color= 'lime', 
                                         decreasing_line_color= 'red',
-                                        name=stock))
-     # chart layout options
+                                        name=stock), row=1, col=1)
+     fig.add_trace(go.Bar(x=hist_df['Date'], y=hist_df['Volume'], showlegend=False), row=2, col=1)
      layout=go.Layout(title= stock, 
                     xaxis=dict(title='Date', 
                     rangeslider=dict(visible=False)), 
                     yaxis=dict(title='Price', showgrid=True, color='lime', gridwidth=1, gridcolor='black'), 
                     plot_bgcolor='slategrey', paper_bgcolor='slategrey')
-     # the chart
-     fig = go.Figure(data=plotly_data, layout=layout)
+     fig.update(layout_xaxis_rangeslider_visible=False)
      fig.show()
 
-# insert stock tickers into array
-stonks = ["ASRT", "DARE", "DNN"] 
+# Insert stock tickers as string into array
+stonks = [] 
 
 moneyprinter(stonks)
-
-
-
